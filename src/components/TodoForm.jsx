b4b5e9de9ab442a721fc4bdbde5247adf90f79b5
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-08-21 22:30:18 -0300

    :sparkles: Creating TodoList Component

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -10,25 +10,25 @@ export default function TodoForm(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // props.onSubmit({
-    //   id: Math.floor(Math.random() * 10000),
-    //   text: input,
-    // });
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
 
     setInput('');
   };
 
   return (
-    <form className='todo-form' onSubmit={handleSubmit}>
+    <form className="todo-form" onSubmit={handleSubmit}>
       <input
-        type='text'
-        placeholder='Add a todo'
+        type="text"
+        placeholder="Add a todo"
         value={input}
-        name='text'
-        className='todo-input'
+        name="text"
+        className="todo-input"
         onChange={handleChange}
       />
-      <button className='todo-button'>Add Todo</button>
+      <button className="todo-button">Add Todo</button>
     </form>
   );
 }
